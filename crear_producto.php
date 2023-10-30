f0337b3504113ecc8dc86021b6531ef6470edeaf
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2023-10-30 12:05:05 +0000

    modificación a crear_producto.php

--- crear_producto.php
+++ crear_producto.php
@@ -3,3 +3,28 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Crear producto</title>
+    </head>
+    <body>
+        <label id="identificador" ></label>
+        <input type="text">
+        <label id="nombre" >Nombre: </label>
+        <input type="text">
+        <label id="precio" >Precio: </label>
+        <input type="text">
+        <fieldset>
+          <legend>Subida de archivos</legend>
+          <label for="file1">Imagen: </label>
+          <input type="file" name="file1" id="file1">
+        </fieldest>
+        <label id="categoria" >Categoría: </label>
+        <input type="text">
+          <button type="submit">Subir datos</button>
+    </body>
+</html>
