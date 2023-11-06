30d7931bf85e5f6dfa1485e422f73e94568a1d43
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2023-11-06 11:52:24 +0000

    modificación del archivo modifica_producto

--- modifica_producto.php
+++ modifica_producto.php
@@ -12,6 +12,23 @@
         <title>Modificar producto</title>
     </head>
     <body>
-        
+        <form action="get">
+            <label for="id">Id: </label>
+            <input type="text" name="id">
+        </form>
+        <form action="procesar_formulario.php" method="post" enctype="multipart/form-data">
+            <label for="nombre">Nombre: </label>
+            <input type="text" name="nombre">
+            <label for="precio">Precio: </label>
+            <input type="text" name="precio">
+            <fieldset>
+                <legend>Subida de archivos</legend>
+                <label for="file1">Imagen: </label>
+                <input type="file" name="file1" id="file1">
+            </fieldset>
+            <label for="categoria">Categoría: </label>
+            <input type="text" name="categoria">
+            <button type="submit">Modificar datos</button>
+        </form>
     </body>
 </html>
