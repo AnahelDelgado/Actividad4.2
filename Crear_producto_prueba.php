f19ab15e1fa3e5f5a3ceff5e2fa5329d56b2a0e7
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2023-11-10 10:15:58 +0000

    Actualización en crear producto prueba

--- Crear_producto_prueba.php
+++ Crear_producto_prueba.php
@@ -84,7 +84,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Si no hay errores, insertar datos en la tabla de productos
     if (empty($errors)) {
         // Mover el archivo a la carpeta de destino
-        $carpeta_destino = "C:/xampp/htdocs/xampp/Actividad3.1/Actividad3.1/Archivos";
+        $carpeta_destino = "C:/xampp/htdocs/xampp/Actividad3.1/Actividad3.1/Archivos/";
         move_uploaded_file($_FILES['file1']['tmp_name'], $carpeta_destino . $nombre_archivo);
 
         // Insertar datos en la tabla de productos
