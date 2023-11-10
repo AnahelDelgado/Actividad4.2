6f5105ca10fe80e4d7af63c1ab95849d5cdf8130
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2023-11-10 10:35:26 +0000

    pequeñas modificaciones

--- modifica_producto.php
+++ modifica_producto.php
@@ -1,7 +1,44 @@
 <?php
 
+    $servername = "localhost";
+    $username = "actividad3.1";
+    $password = "actividad3.1";
+    $database = "actividad3.1";
 
+    try {
+        $conn = new PDO('mysql:host=localhost;dbname=actividad3.1', $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo "Error de conexión: " . $e->getMessage();
+    }
 
+    $id = $_GET['id'];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['boton'])) {
+        $nombre = $_GET['nombre'];
+        $precio = $_GET['precio'];
+        $imagen = $_GET['imagen'];
+        $categoria = $_GET['categoria'];
+    
+        if (!empty($nombreCompleto)) {
+            /* PREPARE */
+            /*
+            $updateStmt = $conn->prepare("UPDATE modulos SET nombre_completo = :nombre_completo, descripcion = :descripcion WHERE iniciales = :iniciales");
+            $updateStmt->bindParam(':nombre_completo', $nombreCompleto);
+            $updateStmt->bindParam(':descripcion', $descripcion);
+            $updateStmt->bindParam(':iniciales', $iniciales);
+            $updateStmt->execute();
+            */
+    
+            /* QUERY */
+            $updateStmt = $conn->query("UPDATE modulos SET nombre_completo = '$nombreCompleto', descripcion = '$descripcion' WHERE iniciales = '$iniciales'");
+    
+            echo "Módulo actualizado correctamente.";
+        } else {
+            echo "El nombre completo es obligatorio.";
+        }
+    }
+    
 ?>
 
 <!DOCTYPE html>
@@ -26,8 +63,10 @@
                 <label for="file1">Imagen: </label>
                 <input type="file" name="file1" id="file1">
             </fieldset>
-            <label for="categoria">Categoría: </label>
+            <label for="categoria">Categoría:</label>
             <input type="text" name="categoria">
+
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
             <button type="submit">Modificar datos</button>
         </form>
     </body>
